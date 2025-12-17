94e203d3fbbce855605bac4e0b8fc1d0c39b929b
minky5004 <minky5004@gmail.com>
2025-12-17 19:30:25 +0900

    20251217 1000번 문제

---
+++ Baekjoon/B1000/Main.java
@@ -0,0 +1,17 @@
+package Baekjoon.B1000;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner stdin = new Scanner(System.in);
+
+        int A = stdin.nextInt();
+        int B = stdin.nextInt();
+
+        System.out.println(A+B);
+
+    }
+
+}
